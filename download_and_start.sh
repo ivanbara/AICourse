2a47818e07261c069d01cd706a66fc44e9659771
ivanbara <frostfiresoft@gmail.com>
2018-01-05 15:27:45 +0200

    Create download_and_start.sh

---
+++ download_and_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## Script that downloads the code for doing the Artificial Intelligence course exercises
+cd /home/jovyan/work
+
+# git reflog requires a name and email if user is not in passwd
+# even if you're only cloning
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+git clone https://github.com/ivanbara/AICourse.git
+
+
+pip install scikit-learn, numpy, scipy, svgwrite, pandas
