9310f738ae40071781b7675f4a0ca5b24797b7a7
ivanbara <frostfiresoft@gmail.com>
2019-02-27 23:42:49 +0200

    Update download_and_start.sh

--- download_and_start.sh
+++ download_and_start.sh
@@ -7,11 +7,6 @@ cd /home/jovyan/work
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
-pip install sklearn numpy scipy svgwrite pandas
-wget "https://github.com/potassco/clingo/releases/download/v5.2.2/clingo-5.2.2-linux-x86_64.tar.gz"
-tar -xzf "clingo-5.2.2-linux-x86_64.tar.gz"
-mv "clingo-5.2.2-linux-x86_64" "clingo"
-
-rm clingo-5.2.2-linux-x86_64.tar.gz
+pip install sklearn numpy scipy svgwrite pandas pymc3
 rm download_and_start.sh
 
